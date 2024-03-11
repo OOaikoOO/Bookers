78aa01e5b3af79c3d7eb7b4fc1b2cba3db946028
OOaikoOO <158584440+OOaikoOO@users.noreply.github.com>
2024-03-11 10:20:16 +0000

    詳細・編集画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'books/new'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/show'
-  get 'books/edit'
+  resources :books
+  get 'books/:id/edit' => 'books#edit'
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
